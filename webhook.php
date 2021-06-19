f4fd5ccbf2340df4583cbffeb0247d5ec5569e08
Andrés F. Londoño <anfelipeloal@gmail.com>
2021-06-19 14:21:03 -0500

    fix(mp): webhook

--- webhook.php
+++ webhook.php
@@ -1,28 +1,32 @@
 <?php
 
-    require_once 'vendor/autoload.php';
+    $json = file_get_contents('php://input');
+    // Converts it into a PHP object
 
+    $logFile = fopen("log_mp_json.txt", 'a') or die("Error creando archivo");
+    fwrite($logFile, print_r($json, true));
+    fclose($logFile);
+
+    require_once 'vendor/autoload.php';
     MercadoPago\SDK::setAccessToken("APP_USR-2572771298846850-120119-a50dbddca35ac9b7e15118d47b111b5a-681067803");
 
-    $webhookNotification = file_get_contents('php://input',true);
+    if ($_GET["topic"] == "payment") {
 
-    switch($_POST["type"]) {
-        case "payment":
-            $payment = MercadoPago\Payment::find_by_id($_REQUEST["id"]);
-            break;
-        case "plan":
-            $plan = MercadoPago\Plan::find_by_id($_REQUEST["id"]);
-            break;
-        case "subscription":
-            $plan = MercadoPago\Subscription::find_by_id($_REQUEST["id"]);
-            break;
-        case "invoice":
-            $plan = MercadoPago\Invoice::find_by_id($_REQUEST["id"]);
-            break;
+        $payment = MercadoPago\Payment::find_by_id($_GET["id"]);
+        $merchant_order = MercadoPago\MerchantOrder::find_by_id($payment->order->id);
+
+        //guardo en un archivo payment
+        $logFile = fopen("log_payment.txt", 'a') or die("Error creando archivo");
+        fwrite($logFile, print_r($payment, true));
+        fclose($logFile);
+
+        //guardo en un archivo merchant_order
+        $logFile = fopen("log_merchant.txt", 'a') or die("Error creando archivo");
+        fwrite($logFile, print_r($merchant_order, true));
+        fclose($logFile);
     }
 
-    file_put_contents('webhookResponse.json',$webhookNotification);
-
+    ob_clean();
 
     http_response_code(200);
 
