c499e92aaec1f08fd3682830419d28e62e8c4ad8
Andrés F. Londoño <anfelipeloal@gmail.com>
2021-06-19 12:44:25 -0500

    fix(mp): ajuste webhook

--- webhook.php
+++ webhook.php
@@ -1,20 +1,17 @@
 <?php
 
-    require_once 'vendor/autoload.php'; // You have to require the library from your Composer vendor folder
-
-    MercadoPago\SDK::setAccessToken("APP_USR-2572771298846850-120119-a50dbddca35ac9b7e15118d47b111b5a-681067803");
-
     $txt = "\n\n" . 'webhook_response:' . json_encode($_GET) . "\n\n";
     file_put_contents("payment_log.txt", $txt, FILE_APPEND);
 
-    $type = $GET["type"];
-    $paymentId = $GET["data_id"];
+    $type = $_GET["type"];
+    $paymentId = $_GET["data_id"];
+
 
     if ($type === 'payment') {
 
-        $payment = MercadoPago\Payment::find_by_id($paymentId);
+        $data = file_get_contents("https://api.mercadopago.com/v1/payments/$paymentId?access_token=APP_USR-2572771298846850-120119-a50dbddca35ac9b7e15118d47b111b5a-681067803");
 
-        file_put_contents("payment_log.txt", json_encode($payment), FILE_APPEND);
+        file_put_contents("payment_log.txt", $data, FILE_APPEND);
 
     }
 
